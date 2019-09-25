6147f35b397d0a26f418ffd3366c16607012cacb
alv2017 <v.alishauskaite@gmail.com>
2019-09-25 18:36:09 +0100

    def setUp() method has been removed from test: method under test set_time_point

--- tests/utils/set_time_point_test.py
+++ tests/utils/set_time_point_test.py
@@ -3,32 +3,7 @@ import datetime
 from YahooFinanceDataLoader.utils import set_time_point
 
 class Test_SetTimePoint(unittest.TestCase):
-    
-    def setUp(self):
-        # current date
-        self.current_date = datetime.date.today()    
-                    
-        current_year = int(self.current_date.year)
-        current_month = int(self.current_date.month)
-        current_day = int(self.current_date.day)
-        strYear = str(current_year).zfill(2)
-        strMonth = str(current_month).zfill(2)
-        strDay = str(current_day).zfill(2)    
-        
-        # current date string 
-        self.current_date_string = "-".join([strYear, strMonth, strDay])
-        # current date timestamp
-        self.current_timestamp = datetime.datetime(current_year, 
-                                           current_month, 
-                                           current_day).timestamp()
-        
-        # date that is greater than current
-        self.greater_than_current_date = self.current_date + datetime.timedelta(days=1)
-        gtcd_year = str( self.greater_than_current_date.year ).zfill(2)
-        gtcd_month = str( self.greater_than_current_date.month ).zfill(2) 
-        gtcd_day = str( self.greater_than_current_date.day ).zfill(2) 
-        self.greater_than_current_date_string = "-".join([gtcd_year, gtcd_month, gtcd_day])
-        
+           
     def test_return_timestamp_correct_date_1(self):
         """
             Description: we test function return value for
