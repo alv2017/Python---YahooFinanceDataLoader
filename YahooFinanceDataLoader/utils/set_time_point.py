854c59275ca22a25532f2cbe82bacfbe5a97f8cc
alv2017 <v.alishauskaite@gmail.com>
2019-09-25 18:31:27 +0100

    Method and related tests have been updated: set_time_point

--- YahooFinanceDataLoader/utils/set_time_point.py
+++ YahooFinanceDataLoader/utils/set_time_point.py
@@ -6,31 +6,20 @@ def set_time_point(dt):
         a timestamp in seconds
         Input: date, format "yyyy-mm-dd"
         Return: date timestamp in seconds since 1970-01-01
-        
-        Error is raised if date is less than '2000-01-01'
-        Error is raised if date is greater than or equal to current date
-            (we are dealing with historical data)
     """
     year, month, day = dt.split("-")  
-    
-    current_time = datetime.datetime.now()
     
     # input date is valid
     try:
         input_time = datetime.datetime(int(year), int(month), int(day)) 
-        # input date is less than 2000-01-01
-        if (input_time.date() < datetime.date(2000, 1, 1)):
-            raise ValueError("The smallest possible date is '2000-01-01'")
-        # input date is greater or equal to current date
-        if (input_time.date() >= current_time.date()): 
-            raise ValueError("Input date is greater than or equal to the current date.")
+
     except ValueError as err:
         raise err
       
     return int(input_time.timestamp())
 
 if __name__ == '__main__':
-    d = '2019-09-15'
+    d = '2019-02-15'
     print(set_time_point(d))
 
     
--- tests/utils/set_time_point_test.py
+++ tests/utils/set_time_point_test.py
@@ -30,35 +30,39 @@ class Test_SetTimePoint(unittest.TestCase):
         self.greater_than_current_date_string = "-".join([gtcd_year, gtcd_month, gtcd_day])
         
     def test_return_timestamp_correct_date_1(self):
+        """
+            Description: we test function return value for
+            known date and known timestamp
+        """
         test_date = '2017-05-12'
         test_timestamp =  1494543600
         self.assertEqual(test_timestamp, set_time_point(test_date))
         
     def test_return_timestamp_correct_date_2(self):
+        """
+            Description: we test function return value for
+            known date and known timestamp
+        """
         test_date = '2018-02-01'
         test_timestamp =  1517443200
         self.assertEqual(test_timestamp, set_time_point(test_date))
-      
-    def test_raise_timestamp_incorrect_currentdate(self):
-        test_date = self.current_date_string
-        
-        self.assertRaises(ValueError, set_time_point, test_date)
-
-    def test_raise_error_incorrect_date_before_min(self):
-        test_date = '1999-04-25'
-        self.assertRaises(ValueError, set_time_point, test_date)
-        
+       
     def test_raise_error_incorrect_date_nonexistent_1(self):
+        """
+            Description: the error is raised when provided
+            date string is not actual date.
+        """
         test_date = '2010-02-31'
         self.assertRaises(ValueError, set_time_point, test_date)
         
     def test_raise_error_incorrect_date_nonexistent_2(self):
+        """
+            Description: the error is raised when provided
+            date string is not actual date.
+        """
         test_date = '2015-15-03'
         self.assertRaises(ValueError, set_time_point, test_date)
-        
-    def test_raise_error_incorrect_date_greater_than_current(self):
-        test_date = self.greater_than_current_date_string
-        self.assertRaises(ValueError, set_time_point, test_date)
+
           
 if __name__ == '__main__':
     unittest.main()
