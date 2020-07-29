98c0c85137ef28b6d0eeec4bb3bae6a27d4664c8
alv2017 <v.alishauskaite@gmail.com>
2020-07-29 14:44:11 +0100

    tests/asset_data/__init__.py has been updated

--- tests/asset_data/__init__.py
+++ tests/asset_data/__init__.py
@@ -1,8 +1,5 @@
-from .tests.asset_data.assetsDataRequest.test_request_initialization import *
-
-from . assetsDataRequest.request_data_download import *
-
-from . assetsDataRequest.request_download_permission import *
-
+from .test_request_initialization import *
+from .request_data_download import *
+from .request_download_permission import *
 from .download_asset_data import *
     
