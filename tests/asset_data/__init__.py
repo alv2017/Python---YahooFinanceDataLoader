27e3e32f59aeab278d84c29013b881a1b8574b56
alv2017 <v.alishauskaite@gmail.com>
2020-07-30 14:56:28 +0100

    tests/asset_data/__init__.py has been updated. Reason: removal of tests

--- tests/asset_data/__init__.py
+++ tests/asset_data/__init__.py
@@ -1,5 +1,4 @@
 from .test_assetDataRequest import test_request_initialization
 from .test_assetDataRequest import test_request_actual_data_download
-from .test_assetDataRequest import request_data_download
 
     
