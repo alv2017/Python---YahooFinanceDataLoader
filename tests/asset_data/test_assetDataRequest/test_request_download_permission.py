1203eb89b05db63e7d970ec8b349a50e624ccd79
alv2017 <v.alishauskaite@gmail.com>
2020-08-03 14:43:55 +0100

    Tests that perform actual data requests were moved to a newly created directory "live_tests".

---
+++ tests/asset_data/test_assetDataRequest/test_request_download_permission.py
@@ -0,0 +1,18 @@
+import unittest
+from YahooFinanceDataLoader.asset_data.assetDataRequest import AssetDataRequest
+
+class TestClass_AssetDataRequest_DownloadPermissionRequest(unittest.TestCase):
+    """
+    """
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_response_error_is_1_on_ConnectionError(self):
+        pass
+    
+    def test_response_error_is_1_on_Timeout(self):
+        pass
+
